5bb783b772012cd6814e587593d76ff0da0c5f95
dennis Kiprono <denniskiprono954@gmail.com>
2020-02-13 10:04:43 -0500

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+function passGender(gender){
+    var birth=document.getElementById("date").value;
+var day= new Date(birth).getDay();
+
+    if(gender == "M"){
+        AkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+        var names = AkanNames(date);
+    }else{
+        AkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+        var names = AkanNames(date);
+
+    }
+}
