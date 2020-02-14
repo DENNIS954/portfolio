c41c619d6763602cef5bbe76dc4921deb5664952
dennis Kiprono <denniskiprono954@gmail.com>
2020-02-14 01:15:52 -0500

    back-end first work

--- static/js/script.js
+++ static/js/script.js
@@ -4,10 +4,12 @@ var day= new Date(birth).getDay();
 
     if(gender == "M"){
         AkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-        var names = AkanNames(date);
+        var names = AkanNames(day;
+            document.getElementById("Akan").value=names;
     }else{
         AkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-        var names = AkanNames(date);
+        var names = AkanNames(day);
+        document.getElementById("Akan").value=names;
 
     }
 }
