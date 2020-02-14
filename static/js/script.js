b59f1c28e2bfd5318609c682b89bfaa7761d67b7
dennis Kiprono <denniskiprono954@gmail.com>
2020-02-14 04:31:52 -0500

    javascript function for the form added

--- static/js/script.js
+++ static/js/script.js
@@ -2,16 +2,12 @@ function passGender(gender){
     var birth = document.getElementById("date").nodeValue;
     var day = new Date(birth).getDay();
 
-
-    if(document.getElementById("gender").checked) {
-        var gender = 'male';
-    }
-    else {
-        var gender = 'female';
-    }
-    if(gender == 'M'){
-        Akannames = ["Kwasi","Kwandwo","Kwaku","Yaw","Kofi","Kwame"];
-        var display = Akannames[day];
-        document.getElementById("Akan").value=display;
-    }
+    if(gender == "M"){
+        Akannames = ["Kwasi", "Kwandwo", "Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+        var names = Akannames[day];
+        document.getElementById("Akan").value=names;
+    }else
+    Akannames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var names = Akannames[day];
+    document.getElementById("Akan").value=names; 
 }
