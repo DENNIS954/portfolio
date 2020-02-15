4cbd5f3840501f1859c35f96679ddf8ed6933a6f
dennis Kiprono <denniskiprono954@gmail.com>
2020-02-14 23:54:20 -0500

    README.md added

---
+++ README.md
@@ -0,0 +1,28 @@
+# Portfolio Landing Page
+
+#### By Dennis Kiprono
+## Description
+This project is a web application that gives out the Akan name depending on your date of birth and the day of the week you were born.
+## Setup/Installation Requirements
+One should have a well functioning computer/laptop with the following;
+1. Atleast 4GB RAM.
+2. Core i5.
+3. 500GB HDD Hard Disk.
+4. Available good internet network.
+5. Available stable power supply.
+
+## Technology used
+The programs used to build the above project include;
+1. HTML
+2. CSS
+3. Javascript
+4. Bootstrap
+
+## Support and Contact Details
+Any query or any issue concerning the above project, don't hesitate to contact;
++ phone: 0722732916
++ email: denniskiprono954@gmail.com
++ facebook: Thee Snowkid
+
+### License
+This project is licensed under the MIT License.Copyright 2020(Dennis Kiprono)
--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,13 @@
-function getName(){
-    var gender = document.getElementById("gender").checked;
-    
-if(gender == true){
-    Akannames = ["Kwasi", "Kwandwo", "Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    var names = Akannames[day];
-    document.getElementById("Akan").value = names;
-    alert("male");
-}else if(gender == false){
-    Akannames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    alert("female");
-}
+function passGender(gender){
+    var birth = document.getElementById("date").nodeValue;
+    var day = new Date(birth).getDay();
 
+    if(gender == "M"){
+        Akannames = ["Kwasi","Kwandwo", "Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+        var names = Akannames[day];
+        document.getElementById("Akan").value=names;
+    }else
+    Akannames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var names = Akannames[day];
+    document.getElementById("Akan").value=names; 
 }
